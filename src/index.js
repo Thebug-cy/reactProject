bc81efeab0b371ac8bb8e87c985abcef9405391c
Thebug-cy <735977249@qq.com>
2020-10-11 15:10:19 +0800

    完善拦截器，代理，视口，字体比值

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//约束字体大小
+import './assets/js/font'
+//通用样式
+import './assets/css/base.css'
+
+import './plugins/axios'
+import './plugins/umi-request'
+
+
 
 ReactDOM.render(
-    'hello',
+    <div>hello</div>,
   document.getElementById('root')
 );
