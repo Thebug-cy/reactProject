5ce41835dd73c4d084087001cabc2575d0bb8c10
Thebug-cy <735977249@qq.com>
2020-10-12 19:13:01 +0800

    解决了问题

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import './assets/js/font'
 //通用样式
 import './assets/css/base.css'
 
+import './plugins/axios'
+import './plugins/umi-request'
 import {BrowserRouter as Router} from 'react-router-dom';
 
-
-const show = () =>{
+const show = () => {
     console.log('leader')
-};
+}
 
 ReactDOM.render(
     <Router>
