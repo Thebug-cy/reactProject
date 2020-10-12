ff2d9dd0140fc3c29f00d18c64014668e3be348b
Thebug-cy <735977249@qq.com>
2020-10-12 17:00:27 +0800

    添加了一个swiper目录，未开发

---
+++ src/components/my-swiper/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from './my-swiper.module.css';
+import swiper from './js/swiper';
+import $ from 'jquery';
+import propTypes from 'prop-types';
+import {withRouter} from 'react-router-dom'
--- src/index.js
+++ src/index.js
@@ -8,9 +8,41 @@ import './assets/css/base.css'
 import './plugins/axios'
 import './plugins/umi-request'
 
+class Test extends React.Component{
+    render() {
+        return(
+            <div className="test">
+                <h3>test</h3>
+                <ul>
+                    <li>xXXXx</li>
+                </ul>
+                <input type="text"/>
+            </div>
+        )
+    }
+    componentDidMount() {
+        this.axios({
+            url:'/api/users',
+            /*params:{
+                _page:1,
+                _limit:2
+            }*/
+        }).then(
+            res => console.log('axios',res)
+        );
 
+        this.request(`/api/users`,{
+            params:{
+                _page:1,
+                _limit:2
+            }
+        }).then(
+            res => console.log('umi',res)
+        )
+    }
+}
 
 ReactDOM.render(
-    <div>hello</div>,
+    <Test/>,
   document.getElementById('root')
 );
